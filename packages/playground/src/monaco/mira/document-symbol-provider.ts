e990eb68865b73b5b565f8421bac4d9673a0070c
OpportunityLiu <opportunity@live.in>
2025-06-08 04:53:57 +0800

    feat: symbols tree

--- packages/playground/src/monaco/mira/document-symbol-provider.ts
+++ packages/playground/src/monaco/mira/document-symbol-provider.ts
@@ -1,9 +1,53 @@
-import { type CancellationToken, type editor, languages } from '@private/monaco-editor';
+import { type CancellationToken, type editor, languages, Range } from '@private/monaco-editor';
 import { Provider } from './worker-helper';
 import { DiagnosticCode } from 'mira-wasm';
+import type { SourceScope } from './compile-result';
 
 /** @inheritdoc */
 class DocumentSymbolProvider extends Provider implements languages.DocumentSymbolProvider {
+    /** 构建树 */
+    private handleScope(model: editor.ITextModel, scope: SourceScope): languages.DocumentSymbol[] {
+        const symbols: languages.DocumentSymbol[] = [];
+        const unhandledChildren = new Set(scope.children);
+        for (const { definition, range } of scope.locals) {
+            let kind: languages.SymbolKind = languages.SymbolKind.Variable;
+            let name: string | undefined;
+            let children: languages.DocumentSymbol[] = [];
+            let allRange = range;
+            // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
+            switch (definition.code) {
+                case DiagnosticCode.UnusedParameterIt:
+                    continue; // 忽略未使用的 it 参数
+                case DiagnosticCode.ParameterIt:
+                    name = `it`;
+                    break;
+                case DiagnosticCode.LocalFunction: {
+                    kind = languages.SymbolKind.Function;
+                    const funcScope = scope.children.find((s) => Range.compareRangesUsingStarts(s.range, range) > 0);
+                    if (funcScope) {
+                        allRange = Range.plusRange(range, funcScope.range);
+                        children = this.handleScope(model, funcScope);
+                        unhandledChildren.delete(funcScope);
+                    }
+                    break;
+                }
+            }
+            symbols.push({
+                name: name ?? model.getValueInRange(definition.range),
+                detail: '',
+                kind,
+                range: allRange,
+                tags: [],
+                selectionRange: range,
+                children: children,
+            } satisfies languages.DocumentSymbol);
+        }
+        // 处理未处理的子作用域
+        for (const child of unhandledChildren) {
+            symbols.push(...this.handleScope(model, child));
+        }
+        return symbols;
+    }
     /** @inheritdoc */
     async provideDocumentSymbols(
         model: editor.ITextModel,
@@ -11,30 +55,9 @@ class DocumentSymbolProvider extends Provider implements languages.DocumentSymbo
     ): Promise<languages.DocumentSymbol[] | undefined> {
         const compiled = await Provider.getCompileResult(model);
         if (!compiled) return undefined;
-        const { locals } = compiled.groupedTags(model);
-        return locals.map(({ definition }) => {
-            let kind: languages.SymbolKind = languages.SymbolKind.Variable;
-            let name: string | undefined;
-            // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
-            switch (definition.code) {
-                case DiagnosticCode.UnusedParameterIt:
-                case DiagnosticCode.ParameterIt:
-                    name = `it`;
-                    break;
-                case DiagnosticCode.LocalFunction:
-                    kind = languages.SymbolKind.Function;
-                    break;
-            }
-            return {
-                name: name ?? model.getValueInRange(definition.range),
-                detail: '',
-                kind,
-                range: definition.range,
-                tags: [],
-                selectionRange: definition.range,
-                children: [],
-            } satisfies languages.DocumentSymbol;
-        });
+        const root = compiled.scopes(model)[0];
+        if (!root) return undefined;
+        return this.handleScope(model, root);
     }
 }
 languages.registerDocumentSymbolProvider('mirascript', new DocumentSymbolProvider());
