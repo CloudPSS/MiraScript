cda40d49ae094f6c9181fc6e59bab5c12e228522
OpportunityLiu <opportunity@live.in>
2025-06-15 02:59:02 +0800

    build: add a doc

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -1,3 +1,5 @@
 fn main() {
+    // Fixed in https://github.com/purpleprotocol/mimalloc_rust/pull/136
+    // Remove this when the next version of mimalloc is released.
     println!("cargo:rustc-link-lib=Advapi32");
 }
