7335abe6d81a813d7142a8f9ddf3c19c572da958
OpportunityLiu <opportunity@live.in>
2025-09-12 17:13:07 +0800

    fix(wasm): loader

--- packages/wasm/src/loader/web.ts
+++ packages/wasm/src/loader/web.ts
@@ -1,19 +1,17 @@
 import type { InitInput } from '../../lib/wasm.js';
 
+/** 回退加载 */
+async function loadFallback() {
+    const fallbackUrl =
+        (document?.currentScript instanceof HTMLScriptElement
+            ? document.currentScript.src
+            : (document.currentScript?.href?.baseVal ?? '')) || document.location.href;
+    return await fetch(new URL('../../lib/wasm_bg.wasm', fallbackUrl));
+}
+
 export const module: Promise<InitInput> = /* @__PURE__ */ (async () => {
-    try {
-        if (import.meta.url) {
-            return await fetch(new URL('../../lib/wasm_bg.wasm?url', import.meta.url));
-        }
-        const fallbackUrl =
-            (document?.currentScript instanceof HTMLScriptElement
-                ? document.currentScript.src
-                : (document.currentScript?.href?.baseVal ?? '')) || document.location.href;
-        return await fetch(new URL('../../lib/wasm_bg.wasm?url', fallbackUrl));
-    } catch {
-        // @ts-expect-error load as module
-        const mod = (await import('../../lib/wasm_bg.wasm?url')) as { default: unknown };
-        if (typeof mod.default == 'string' || mod.default instanceof WebAssembly.Module) return mod.default;
-        return mod;
+    if (!import.meta.url) {
+        return await loadFallback();
     }
+    return await fetch(new URL('../../lib/wasm_bg.wasm?url', import.meta.url));
 })();
