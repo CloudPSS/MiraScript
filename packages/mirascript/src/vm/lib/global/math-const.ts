7350aad171c3c14185ab8cdf32d534f915cef410
OpportunityLiu <opportunity@live.in>
2025-12-02 16:38:17 +0800

    feat: 修改常量

--- packages/mirascript/src/vm/lib/global/math-const.ts
+++ packages/mirascript/src/vm/lib/global/math-const.ts
@@ -1,2 +1,34 @@
-const { PI, E } = Math;
-export { PI as '@pi', E as '@e' };
+import { VmLib } from '../helpers.js';
+
+export const PI = VmLib(Math.PI, {
+    summary: '圆周率',
+    returnsType: 'number',
+});
+export const E = VmLib(Math.E, {
+    summary: '自然对数的底数',
+    returnsType: 'number',
+});
+export const SQRT1_2 = VmLib(Math.SQRT1_2, {
+    summary: '1 的平方根除以 2',
+    returnsType: 'number',
+});
+export const SQRT2 = VmLib(Math.SQRT2, {
+    summary: '2 的平方根',
+    returnsType: 'number',
+});
+export const LN2 = VmLib(Math.LN2, {
+    summary: '2 的自然对数',
+    returnsType: 'number',
+});
+export const LN10 = VmLib(Math.LN10, {
+    summary: '10 的自然对数',
+    returnsType: 'number',
+});
+export const LOG2E = VmLib(Math.LOG2E, {
+    summary: 'e 以 2 为底的对数',
+    returnsType: 'number',
+});
+export const LOG10E = VmLib(Math.LOG10E, {
+    summary: 'e 以 10 为底的对数',
+    returnsType: 'number',
+});
