9a7e082ff91951917dbd3a19d9be7725d690068b
OpportunityLiu <opportunity@live.in>
2025-12-05 23:48:48 +0800

    perf: 减少插桩检查

--- packages/mirascript/src/vm/checkpoint.ts
+++ packages/mirascript/src/vm/checkpoint.ts
@@ -1,8 +1,9 @@
+/* eslint-disable unicorn/prefer-math-trunc */
 import { isNaN } from '../helpers/utils.js';
 // 不使用 performance.now()，精度够即可，且性能开销更小
 const { now } = Date;
-const TIME_ORIGIN = now() - 1000 * 3600 * 24; // 减去一天，防止系统时间被调整到过去时出问题
-const timestamp = () => now() - TIME_ORIGIN;
+const TIME_ORIGIN = now() - 1000 * 3600 * 12; // 防止系统时间被调整到过去时出问题
+const timestamp = () => (now() - TIME_ORIGIN) | 0;
 
 const CP_DEFAULT_INTERVAL = 100; // 每 100 次调用检查一次
 const MAX_DEPTH = 128;
@@ -11,31 +12,33 @@ const CP_UNSET = -1;
 const CP_DEFAULT_TIMEOUT = 100;
 
 let cpDepth = 0;
-let cp = CP_UNSET;
-let cpTimeout = CP_DEFAULT_TIMEOUT;
-let cpInterval = CP_DEFAULT_INTERVAL;
-let cpCount = 0;
+let cp = CP_UNSET | 0;
+let cpTimeout = CP_DEFAULT_TIMEOUT | 0;
+let cpInterval = CP_DEFAULT_INTERVAL | 0;
+let cpCounter = 0;
 /** 检查点 */
 export function Cp(): void {
     if (cp === CP_UNSET) {
-        cp = timestamp();
+        cpCounter = 0;
+        cp = timestamp() | 0;
         return;
     }
-    cpCount++;
-    if (cpCount >= cpInterval) {
-        cpCount = 0;
-        if (timestamp() - cp >= cpTimeout) {
-            throw new RangeError('Execution timed out');
-        }
+    // 不是每次都查时间
+    if ((cpCounter = (cpCounter + 1) | 0) % cpInterval !== 0) {
+        return;
+    }
+    cpCounter = 0;
+    if ((timestamp() | 0) - (cp | 0) >= (cpTimeout | 0)) {
+        throw new RangeError('Execution timed out');
     }
 }
 /** 检查点 */
 export function CpEnter(): void {
-    cpDepth++;
-    if (cpDepth <= 1) {
-        cp = timestamp();
+    cpDepth = (cpDepth | 0) + 1;
+    if ((cpDepth | 0) <= 1) {
+        cp = timestamp() | 0;
         cpDepth = 1;
-    } else if (cpDepth > MAX_DEPTH) {
+    } else if ((cpDepth | 0) > MAX_DEPTH) {
         throw new RangeError('Maximum call depth exceeded');
     } else {
         Cp();
@@ -43,25 +46,27 @@ export function CpEnter(): void {
 }
 /** 检查点 */
 export function CpExit(): void {
-    cpDepth--;
-    if (cpDepth < 1) {
-        cp = CP_UNSET;
+    cpDepth = (cpDepth | 0) - 1;
+    if ((cpDepth | 0) < 1) {
+        cp = CP_UNSET | 0;
         cpDepth = 0;
     } else {
         Cp();
     }
 }
+
+const INT_MAX = 0x7fff_ffff;
 /** 设置检查点超时时间 */
 export function configCheckpoint(
     timeout: number = CP_DEFAULT_TIMEOUT,
     checkInterval: number = CP_DEFAULT_INTERVAL,
 ): void {
-    if (typeof timeout !== 'number' || timeout <= 0 || isNaN(timeout)) {
+    if (typeof timeout !== 'number' || timeout <= 0 || timeout >= INT_MAX || isNaN(timeout)) {
         throw new RangeError('Invalid timeout value');
     }
-    if (typeof checkInterval !== 'number' || checkInterval <= 0 || isNaN(checkInterval)) {
+    if (typeof checkInterval !== 'number' || checkInterval <= 0 || checkInterval >= INT_MAX || isNaN(checkInterval)) {
         throw new RangeError('Invalid check interval value');
     }
-    cpTimeout = Math.ceil(timeout);
-    cpInterval = Math.ceil(checkInterval);
+    cpTimeout = timeout | 0;
+    cpInterval = checkInterval | 0;
 }
