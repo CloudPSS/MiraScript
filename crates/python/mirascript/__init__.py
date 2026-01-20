79e9e5297a6a0c2df17e26fe55a63eea0faa7406
zdm <zdm_3692@163.com>
2026-01-20 17:24:54 +0800

    Bump version to 0.0.4

--- crates/python/mirascript/__init__.py
+++ crates/python/mirascript/__init__.py
@@ -7,5 +7,5 @@ This module provides the main entry point for compiling MiraScript code
 from .mirascript import Config
 from .diagnostics import Diagnostic
 from .main import compile
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __all__ = ["Config", "Diagnostic", "compile","__version__"]
