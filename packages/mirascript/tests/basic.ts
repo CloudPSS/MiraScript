d8f01a97cdf9114f82bfd421d83c13e22bcca7fe
zhouyong <zhouyong@cloudpss.net>
2026-01-26 17:33:20 +0800

    test: type 支持尾随逗号

--- packages/mirascript/tests/basic.ts
+++ packages/mirascript/tests/basic.ts
@@ -98,6 +98,8 @@ test('whitespaces', compileAndRun, ' \n', null);
 test('identifier', compileAndRun, 'PI', Math.PI);
 test('identifier with whitespace', compileAndRun, ' \n SQRT1_2\n\r\n ', Math.SQRT1_2);
 
+test('type with trailing comman', compileAndRun, 'type(1,)', 'number');
+
 test('keyword', compileAndRun, 'if', {
     instanceOf: Error,
     message: /MissingCloseBrace/,
