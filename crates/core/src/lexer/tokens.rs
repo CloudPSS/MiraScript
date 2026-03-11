37e0e3af883bc0980259c6aead0ed1f5c2e625fc
OpportunityLiu <opportunity@live.in>
2026-03-11 16:10:08 +0800

    feat(core): 支持 `type` 作为标识符使用

--- crates/core/src/lexer/tokens.rs
+++ crates/core/src/lexer/tokens.rs
@@ -127,14 +127,20 @@ pub(super) fn token<'s>(
     )
     .parse_next(input)?;
 
-    if matches!(prev_token.as_deref(), Some(prev_token) if prev_token.is_keyword()) {
-        let Some(prev_token) = prev_token else {
-            unreachable!();
-        };
-        let TokenKind::Keyword(kw) = prev_token.kind else {
-            unreachable!();
-        };
+    if let Some(prev_token) = prev_token
+        && let TokenKind::Keyword(kw) = prev_token.kind
+    {
         if cur_token == Operator::Colon || cur_token == Operator::QuestionColon {
+            // `?:` 和 `:` 之前的标识符是字段名称，而不是关键字。
+            prev_token.kind = TokenKind::Identifier(kw.into());
+        } else if kw == Keyword::Type
+            && !(cur_token == Operator::OpenParen
+                || cur_token == Operator::OpenBrace
+                || cur_token.is_identifier())
+        {
+            // `type()` 关键字必须作为类函数调用使用，否则 `type` 是一个普通标识符。
+            // 不支持使用 `fn type() {}` 或 `fn type {}` 定义函数，因此 `type` 标识符后面不能是 `(`、`{`。
+            // 预留 type id {} 语法，允许 type 后面直接跟一个标识符作为类型定义。
             prev_token.kind = TokenKind::Identifier(kw.into());
         }
     }
--- packages/mirascript/tests/snippets/type.ts
+++ packages/mirascript/tests/snippets/type.ts
@@ -11,3 +11,11 @@ tw('array', 'type([])', 'array');
 tw('record', 'type(())', 'record');
 
 tw('with trailing comma', 'type(1,)', 'number');
+
+tw('type as identifier', 'let type = 1; type(type)', 'number');
+tw('type as field name', 'let x = (type: 1); x.type', 1);
+tw('type as field name with ?:', 'let x = (type?: 1); x.type', 1);
+tw('type as function name', 'fn type() { return 1; }', /InvalidTypeCall/);
+tw('type as function name with it args', 'fn type { return it; }', /MissingOpenParenAfterType/);
+tw('call type with same name identifier', 'let type = fn {1};type(1)', 'number');
+tw('call type identifier', 'let type = fn {1};type!(1)', 1);
