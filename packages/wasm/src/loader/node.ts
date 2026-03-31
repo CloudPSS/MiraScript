88e703299b50359d0083bc5c645bd1e79c5123d6
zhouyong <zhouyong@cloudpss.net>
2026-03-31 18:49:40 +0800

    fix: electron loader

--- packages/wasm/src/loader/node.ts
+++ packages/wasm/src/loader/node.ts
@@ -1,4 +1,5 @@
 import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
 
-const file = /* @__PURE__ */ new URL('../../lib/wasm_bg.wasm', import.meta.url);
+const file = /* @__PURE__ */ fileURLToPath(new URL('../../lib/wasm_bg.wasm', import.meta.url));
 export const module: Promise<BufferSource> = /* @__PURE__ */ fs.readFile(file);
