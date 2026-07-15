394b01dfe3a4aa2ade121f4fce7f3c209d3cdac9
OpportunityLiu <opportunity@live.in>
2026-07-15 12:33:26 +0800

    test(typed): [skip-ci] e2e

---
+++ packages/typed/tests/e2e.ts
@@ -0,0 +1,43 @@
+import test from 'ava';
+import { parse } from '../dist/index.js';
+import { lib } from '@mirascript/mirascript/subtle';
+import type { VmLibOption } from '../../mirascript/src/vm/lib/helpers.ts';
+
+const lt = test.macro<[name: string, param: string, value: VmLibOption]>({
+    exec: (t, name, param, value) => {
+        if (param === 'returns') {
+            t.assert(parse(value.returns?.type ?? 'nil'));
+            return;
+        }
+        const p = value.params?.[param]?.type ?? 'nil';
+        t.assert(parse(p));
+    },
+    title: (t, name, param, value) => {
+        let type: string;
+        if (param === 'returns') {
+            type = value.returns?.type ?? 'nil';
+        } else {
+            type = value.params?.[param]?.type ?? 'nil';
+        }
+        return `parse lib ${name} > ${param} (${type})`;
+    },
+});
+
+type Lib = {
+    [name: string]: VmLibOption | Lib;
+};
+function testLib(lib: Lib) {
+    for (const [name, value] of Object.entries(lib)) {
+        if ('summary' in value || 'params' in value || 'returns' in value) {
+            for (const param of Object.keys(value.params ?? {})) {
+                test(lt, name, param, value as VmLibOption);
+            }
+            if (value.returns) {
+                test(lt, name, 'returns', value as VmLibOption);
+            }
+        } else {
+            testLib(value as Lib);
+        }
+    }
+}
+testLib(lib);
