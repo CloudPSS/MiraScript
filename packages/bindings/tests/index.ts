4d02583f49be02618d675a3d0eb6d4c768b47a90
OpportunityLiu <opportunity@live.in>
2026-08-31 18:24:10 +0800

    test: use cobertura

--- packages/bindings/tests/index.ts
+++ packages/bindings/tests/index.ts
@@ -1,21 +1,21 @@
 import test from 'ava';
 
-import { loadModule } from '../dist/index.js';
-import { loadModule as loadNapi } from '../dist/napi.js';
-import { loadModule as loadWasm } from '../dist/wasm.js';
+import * as bindings from '../dist/index.js';
+import * as napi from '../dist/napi.js';
+import * as wasm from '../dist/wasm.js';
 
 test('loadModule', async (t) => {
-    const module = await loadModule();
+    const module = await bindings.loadModule();
     t.truthy(module);
     t.truthy(module.compileSync);
 
-    const moduleNapi = await loadNapi();
+    const moduleNapi = await napi.loadModule();
     t.truthy(moduleNapi);
     t.truthy(moduleNapi.compile);
     t.truthy(moduleNapi.compileSync);
     t.truthy(moduleNapi.JsCompileResult);
 
-    const moduleWasm = await loadWasm();
+    const moduleWasm = await wasm.loadModule();
     t.truthy(moduleWasm);
     t.truthy(moduleWasm.compileSync);
     t.truthy(moduleWasm.createConfig);
@@ -23,8 +23,16 @@ test('loadModule', async (t) => {
     t.truthy(moduleWasm.wasm);
 });
 
+test('getModule', async (t) => {
+    for (const module of [bindings, napi, wasm]) {
+        const loaded = await module.loadModule();
+        const gotten = module.getModule();
+        t.is(loaded, gotten);
+    }
+});
+
 test('WASM range formatting preserves template literal content', async (t) => {
-    const module = await loadWasm();
+    const module = await wasm.loadModule();
     const expression = 'if active { "活跃" } else { "非活跃" }';
     for (const source of [`<p>状态: \${\n  ${expression}\n}</p>`, `<p>状态: \${\n\n  ${expression}\n}</p>`]) {
         const start = source.indexOf(expression);
